d6e52b4693fc6d5619fda8abb63d32ff7ae0fb72
luisdeseta <luis.deseta@gmail.com>
2021-05-07 00:07:18 -0300

    primeros cambios modo dark

--- script/main.js
+++ script/main.js
@@ -1,1 +1,8 @@
-/**cambio de theme */
+/**dark theme button*/
+const darkButton = document.getElementById("darkSwitch");
+
+darkButton.addEventListener("click", () =>{
+    const classArray = document.body.classList;
+    darkButton.innerText = classArray.contains("theme--dark") ? "Modo Nocturno" : "Modo Diurno";
+    classArray.toggle("theme--dark");
+});
