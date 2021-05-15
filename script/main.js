5a33ea1137f8ef93acc28b7bf0fffe1adb6f3002
luisdeseta <luis.deseta@gmail.com>
2021-05-14 22:14:21 -0300

    primeras lineas JS

--- script/main.js
+++ script/main.js
@@ -6,3 +6,20 @@ darkButton.addEventListener("click", () =>{
     darkButton.innerText = classArray.contains("theme--dark") ? "Modo Nocturno" : "Modo Diurno";
     classArray.toggle("theme--dark");
 });
+
+const apiKey = "JO9qaDVGDEpF16a1uNsl7AEUnAjD9Yed"  //${apiKey}`
+//console.log(data.data[0].images.original.url))
+const trendingTxt = () =>{
+  return new Promise((resolve, reject)=>
+  fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit:3`)
+    .then(response => response.json())
+    .then(data => 
+      document.querySelector(".trendingText").innerHTML =
+      `
+      <img src="${data.data[0].images.original.url}" alt="test1">
+      
+      `
+      )
+  
+  )};
+//trendingTxt()
