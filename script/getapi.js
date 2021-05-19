4b820eb5b65db89a57ffc852a7cb7ba9038b025a
luisdeseta <luis.deseta@gmail.com>
2021-05-19 01:02:58 -0300

    peticion de autocomplete ok. TODO. pintar el html

--- script/getapi.js
+++ script/getapi.js
@@ -1,17 +1,31 @@
-import { APIKEY, SEARCH } from './variables.js';
+import { APIKEY, AUTOCOMPLETE, SEARCH } from './variables.js';
 
 /**
  * @description obtener json del endpoint search
- * @search parametro a buscar
+ * @search parametro a buscar (obligatorio)
  * @limit cantidad de resultados a mostrar
  * @offset orden dentro del json
  */
-export const getSearchEndP = (search,limit,offset) =>{
+export const getSearchEndP = (search,limit,offset=5) =>{
     return new Promise((resolve, reject) =>{
         fetch(`${SEARCH}?api_key=${APIKEY}&q=${search}&limit=${limit}&offset=${offset}`)
         .then(res => res.json())
         .then(data => resolve(data))
         .catch(err => reject(err))
 
+    })
+}
+
+/**
+* @description autocompletar el campo de busqueda
+* @search parametro a buscar (obligatorio)
+*/
+
+export const autoComplete = (search) =>{
+    return new Promise ((resolve, reject)=>{
+        fetch(`${AUTOCOMPLETE}?api_key=${APIKEY}&q=${search}&limit=4`)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
     })
 }
--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-import { getSearchEndP } from './getapi.js';
+import { autoComplete, getSearchEndP } from './getapi.js';
 
 /**dark theme button*/
 const darkButton = document.getElementById("darkSwitch");
@@ -9,22 +9,64 @@ darkButton.addEventListener("click", () =>{
     classArray.toggle("theme--dark");
 });
 
-//busqueda
-// autocompletado
+/**
+ * constantes
+ */
 const inputTextSearch = document.querySelector('#inputSearch')
-console.log(inputTextSearch.value)
-
-const searchDo = () => {
-  alert("hola")  
-} 
-
-
+console.log("trae el campo de busqueda ", inputTextSearch.value)
 const search = inputTextSearch.value;
 const offset = 5
+const ulAutoComplete = document.querySelector('#searchBox-autocomplete');
+//console.log("trae el campo de UL ", ulAutoComplete)
+/**
+ * Listeners
+ */
+inputTextSearch.addEventListener("keypress", (z) => autoComplete(z))
+
+
+//busqueda
 getSearchEndP(search, 12, offset)
 .then((res) => {
-  console.log(res)
+  console.log("trae el endpoint del SEARCH", res)
 })
 .catch((err) =>{
   console.warn('Error al hacer la petición', err)
 })
+
+
+/**
+ * autocompletado
+ * llama al endpoint Autocomplete
+ * recibe el parametro de busqueda
+ * dibuja el HTML 
+ */
+autoComplete(search)
+.then((res) => {
+  console.log("trae el endpoint de AUTOCOMPLE", res)
+  const {data} = res;
+  let sugestSearch ="";
+  let sugestArr =[];
+  for (let i = 0; i < data.length; i++) {
+    sugestArr.push(data[i]);
+    
+  }
+  for (let e = 0; e < sugestArr.length; e++) {
+    sugestSearch += markUpAutoComplet(sugestArr[e].name);
+    
+  }
+  ulAutoComplete.innerHTML = sugestSearch;
+  console.log("trae sugestArr", sugestArr)
+})
+.catch((err) =>{
+  console.warn('Error al hacer la petición', err)
+})
+
+/**
+ * @description texto ingresado del InputText en form busqueda 
+ * @returns dibuja el html
+ */
+const markUpAutoComplet = (sugest) =>{
+   return `
+   <li class="searchBox-autocomplete-list">${sugest}</li>
+  `
+}
